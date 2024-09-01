d1161c31d238d0a8171af4ef65d272d54188f09c
WANG CHENFEI <barrycf.wang@gmail.com>
2024-09-02 06:30:26 +0800

    添加QP求解器

--- develop.cpp
+++ develop.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
 #include <cmath>
-
 #include "FastMath.hpp"
+#include "Algorithm/quadprog.hpp"
 
 int main() {
 
     using namespace FastMath;
-    using namespace FastMath::Impl;
-    Matrix<double, 3, 3> A;
-    double theta = 1;
-    A(0,0) = cos(theta);
-    A(0,1) = sin(theta);
-    A(1,0) = -sin(theta);
-    A(1,1) = cos(theta);
-    A(2,2) = 1;
+    using namespace FastMath::Algorithm;
 
-    std::cout << A << std::endl;
-    std::cout << A.logm() << std::endl;
+    SquareMatrix<double, 2> H = {1, -1, -1, 2};
+    Vector2d f = {-2, -6};
+    Matrix<double, 3, 2> A = {1,1,-1,2,2,1};
+    Vector3d b = {2,2,3};
+
+    Matrix<double, 0, 2> Ae;
+    Vector<double, 0> be;
+
+    Vector<double, 2> x;
+
+    std::cout << quadprog<2, 3, 0>(H, f, A, b, Ae, be, x) << std::endl;
+    std::cout << x << std::endl;
 
     return 0;
 
