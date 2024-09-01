b5a467d6396663ed014027826e077ac14d9ab6a2
WANG CHENFEI <barrycf.wang@gmail.com>
2024-09-01 22:35:33 +0800

    更新demo

--- develop.cpp
+++ develop.cpp
@@ -7,12 +7,13 @@ int main() {
 
     using namespace FastMath;
     using namespace FastMath::Impl;
-    Matrix<double, 2, 2> A;
+    Matrix<double, 3, 3> A;
     double theta = 1;
     A(0,0) = cos(theta);
     A(0,1) = sin(theta);
     A(1,0) = -sin(theta);
     A(1,1) = cos(theta);
+    A(2,2) = 1;
 
     std::cout << A << std::endl;
     std::cout << A.logm() << std::endl;
